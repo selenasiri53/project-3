8bf4b107197aee4be99ab5a44b724449417836ea
Selena Siri <selenasiri@gmail.com>
2022-11-11 15:29:10 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <h1>Reactivity in Action</h1>
+  </header>
+  <section id="app">
+    <button @click="add(5)">Add 5</button>
+    <button @click="add(1)">Add 1</button>
+    <!-- 1) Connect the buttons and calculate a value (a number) -->
+    <!-- Show "Not there yet" until you reach a result of exactly 37 -->
+    <!-- Show "Too much!" if the result is greater than 37 -->
+    <p>Result: {{ number }}</p>
+    <!-- 2) Watch for changes in "result" and reset the value to 0 after 5 seconds -->
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  data() {
+    return {
+      number: 0,
+    };
+  },
+  // computed: {
+  //     if (this.number < 37) {
+  //       return 'Not there yet!';
+  //     } else if (this.number > 37) {
+  //       return 'Too much!';
+  //     } else {
+  //       return this.number;
+  //     }
+  // },
+  // watch: {
+  //   result(value, oldValue) {
+  //     setTimeout(() => {
+  //       this.number = 0;
+  //     }, 5000);
+  //   },
+  // },
+  methods: {
+    add(num) {
+      this.number = this.number + num;
+    },
+  },
+};
 </script>
 
 <style>
